3b18157091ed107b6ddfba65d3103be796919bca
Myakabuka <mykabuka6@gmail.com>
2017-07-14 00:01:16 +0600

    New Files upload. Trying to do something not useless.
    I'am really trying.

---
+++ CoddingDZ - Lesson1/CoddingDZ - Lesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoddingDZ___Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Or Oleg. Hello for you too.");
+        }
+    }
+}
